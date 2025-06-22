850085d34f1ef2d3048447e41dc366134aa5373f
SDHINSA <sohanad@icloud.com>
2025-06-22 17:25:08 -0600

    Search functionality is complete

---
+++ src/components/List.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+  
+  function List({searchItem, allProducts}) {
+
+    const filteredData = allProducts.filter((el) =>{
+        
+        // No search 
+        if (searchItem === ''){
+            return true;
+        }
+        
+        else {
+            return (
+                // Use the questoin marks after because it won't crash then
+                el.name?.toLowerCase().includes(searchItem) ||
+                el.brand?.toLowerCase().includes(searchItem) ||
+                el.type?.toLowerCase().includes(searchItem)
+            )
+            
+
+        }
+    })
+
+
+
+      return (
+          <ul>
+              {filteredData.map((item) => (
+                  <li key={item.id}>{item.brand} - {item.name}</li>
+              ))}
+          </ul>
+      )
+  }
+  
+  export default List
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import { useOutletContext } from "react-router-dom";
+import List from "../components/List.jsx"
+import TextField from "@mui/material/TextField";
+import '../styles/Shop.css'
 
 
 
 function Shop(){
     const { addToCart } = useOutletContext();
-    const [allProducts, setProducts] = useState()
-    const [search, setSearchItem] = useState();
+    const [allProducts, setProducts] = useState([])
+    const [searchItem, setSearchItem] = useState('');
 
     //  Grab the products 
     useEffect( () => {
@@ -17,6 +20,7 @@ function Shop(){
                 .filter(product => product.image_link) // only keep products with an image
                 .map(product => ({
                     brand: product.brand,
+                    name: product.name,
                     image: product.image_link,
                     number: 0,
                     id: product.id,
@@ -33,12 +37,33 @@ function Shop(){
         });
     }, []); // dont want to refetch based on filter so we good 
 
-    console.log(allProducts);
+    // Search bar implementation
+    let inputHandler = (e) => {
+        var lowerCase = e.target.value.toLowerCase();
+        setSearchItem(lowerCase);
+    }
+
+    // console.log(allProducts);
 
 
     return (
         <>
             <h1>This is the Shopping Page</h1>
+            
+            <div className="main">
+            <h1>React Search</h1>
+            <div className="search">
+                <TextField
+                id="outlined-basic"
+                onChange={inputHandler}
+                variant="outlined"
+                fullWidth
+                label="Search"
+                />
+            </div>
+            <List searchItem={searchItem} allProducts={allProducts}/>
+            </div>
+  
         </>
     )
 }
